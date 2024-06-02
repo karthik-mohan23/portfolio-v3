d3f42d8c3f655b743a7dc22c45724a367ea0ba08
karthik-mohan23 <karthikmohan2310@gmail.com>
2024-06-02 19:39:47 +0530

    Built landing page

---
+++ src/app/pages/Hero.tsx
@@ -0,0 +1,23 @@
+import { AuroraBackground } from "../components/ui/aurora-background";
+
+function Hero() {
+  return (
+    <AuroraBackground>
+      <div className="">
+        <h1 className="text-white font-bold text-7xl max-w-3xl text-center">
+          Hi, my name is KARTHIK MOHAN.
+        </h1>
+        <p className="text-white text-center text-2xl py-5">
+          Full Stack Developer
+        </p>
+        <div className="flex justify-center">
+          <button className="bg-white px-4 py-2 rounded-full hover:cursor-pointer">
+            Contact
+          </button>
+        </div>
+      </div>
+      <p>And this, is chemical burn.</p>
+    </AuroraBackground>
+  );
+}
+export default Hero;

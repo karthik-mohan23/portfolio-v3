693c27388d34f5399e91b3e23194045f4267da64
karthik-mohan23 <karthikmohan2310@gmail.com>
2024-06-02 21:22:49 +0530

    fix skill card height issue

--- src/app/components/ui/GlowingCard.tsx
+++ src/app/components/ui/GlowingCard.tsx
@@ -13,18 +13,20 @@ export function BackgroundGradientDemo({
   return (
     <div className="lg:w-52 w-32 ">
       <BackgroundGradient className="rounded-[22px] px-1 py-4 lg:p-4  bg-white dark:bg-zinc-900">
-        <div className="flex justify-center">
-          <Image
-            src={image}
-            alt="jordans"
-            height="50"
-            width="50"
-            className=" object-contain"
-          />
+        <div className="flex flex-col justify-center items-center gap-2">
+          <div className="flex justify-center">
+            <Image
+              src={image}
+              alt="jordans"
+              height="50"
+              width="50"
+              className="aspect-square"
+            />
+          </div>
+          <p className="text-xs lg:text-lg text-center text-white mt-4 mb-2">
+            {label}
+          </p>
         </div>
-        <p className="text-xs lg:text-lg text-center text-white mt-4 mb-2">
-          {label}
-        </p>
       </BackgroundGradient>
     </div>
   );
